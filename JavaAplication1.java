599dc98fa542634b01e3728bae85d541871074b9
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-03-01 06:51:03 +0000

    Update changes

---
+++ JavaAplication1.java
@@ -0,0 +1,14 @@
+public class JavaAplication1 {
+    public static void main(String[] args) {
+        System.out.println("\nHello World");
+        System.out.println("Esto es una prueba de edicion");
+
+        int a, b, c;
+
+        a = 4;
+        b = 5;
+        c = a + b;
+
+        System.out.println("El valor de x es: " + c);
+    }
+}
